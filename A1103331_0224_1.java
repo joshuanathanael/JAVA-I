d4c34d55643849ded296970624c54521bb22a0ca
李勇安 A1103331 <125531951+joshuanathanael@users.noreply.github.com>
2023-03-02 09:24:46 +0800

    Add files via upload

---
+++ A1103331_0224_1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class A1103331_0224_1{
+       public static void main(String[] argv){
+      
+       int input;
+       System.out.println("Please enter the integer : ");
+       Scanner sc = new Scanner(System.in);
+       input=sc.nextInt();
+
+       System.out.println((input%2) == 0 ? "even" : "odd");
+
+       }
+}
